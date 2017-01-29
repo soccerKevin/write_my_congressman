477010dc2018a8d57dc42ff305bfc48db7cb2b01
soccerkevin <soccerkevint11@gmail.com>
2017-01-29 15:41:23 -0800

    refactor google_civic

--- lib/tasks/google_api.rake
+++ lib/tasks/google_api.rake
@@ -7,5 +7,5 @@ require_relative '../vendor_api/google_civic_api.rb'
 task :get_address => :environment do
   include VendorAPI
   address = StreetAddress::US.parse("14531 31st ave ne Seattle, WA, 98155")
-  GoogleCivic.get_from_address address
+  pp GoogleCivic.get_from_address address
 end
--- lib/vendor_api/google_civic_api.rb
+++ lib/vendor_api/google_civic_api.rb
@@ -4,31 +4,32 @@ module VendorAPI
   class GoogleCivic
     @@KEY = Rails.application.secrets.google_civics_api_key
     @@ROOT = 'https://www.googleapis.com/civicinfo/v2'
+    @@WHITELISTED_OFFICES = ['senate', 'house', 'whitehouse']
 
     class << self
       def get_from_address(address)
         address = sanitize_address address
         query = { address: address.to_s, key: @@KEY }.to_query
-        response_names HTTParty.get "#{@@ROOT}/representatives?#{query}"
+        response = HTTParty.get "#{@@ROOT}/representatives?#{query}"
+        officials = whitelist_legislators response
+        names = officials.map{ |official| official['name'] }
       end
 
     private
 
       def sanitize_address(address)
-        StreetAddress::US.parse address unless address.is_a? StreetAddress::US::Address
+        address.is_a?(StreetAddress::US::Address) ? address : StreetAddress::US.parse(address)
       rescue Exception => e
         raise e.is_a?(TypeError) ? ArgumentError.new("argument must be an instance of StreetAddress") : e
       end
 
-      def response_names(response)
-        offices = ['senate', 'house', 'whitehouse']
+      def whitelist_legislators(response)
         officials = response['officials'].select do |official|
-          offices.select do |office|
+          @@WHITELISTED_OFFICES.map do |office|
             next unless official['urls']
-            official['urls'].first.include? office rescue binding.pry
-          end
+            official['urls'].first.include? office
+          end.include? true
         end
-        names = response['officials'].map{ |official| official['name'] }
       end
     end
   end
