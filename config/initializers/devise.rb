0f72e5ab3dec7c3ab3bc067d7300f057999330bb
soccerkevin <soccerkevint11@gmail.com>
2017-02-06 22:54:38 -0800

    stop omniauth warning

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -1,3 +1,4 @@
+Hashie.logger = Logger.new(nil)
 # Use this hook to configure devise mailer, warden hooks and so forth.
 # Many of these configuration options can be set straight in your model.
 Devise.setup do |config|
