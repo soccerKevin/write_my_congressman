d9dca04ba9b336a9fb101d82e2e13d6ea4c696d5
soccerkevin <soccerkevint11@gmail.com>
2017-02-21 17:42:54 -0800

    create migrations for topics and messages

---
+++ db/migrate/20170222013706_create_messages.rb
@@ -0,0 +1,10 @@
+class CreateMessages < ActiveRecord::Migration
+  def change
+    create_table :messages do |t|
+      t.references :topic
+      t.references :user
+      t.string :subject
+      t.text :body
+    end
+  end
+end
