d0880dea39f4cc184ea8de6ab2c29e0f102b2563
soccerkevin <soccerkevint11@gmail.com>
2017-03-18 16:24:32 -0700

    remove guest user, re-setup message form:

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -2,9 +2,22 @@ class MessagesController < ApplicationController
   layout 'message'
 
   def new
-    @user = current_user
+    @user = current_user || User.new
     @message = @user.messages.build
     @user.address || @user.build_address
     @legislators = @user.legislators.reject{ |l| %w{trump pence}.include? l.last_name.downcase }
+  end
+
+  helper_method def placeholders
+    @placeholders = {
+      name: current_user&.name || true,
+      email: current_user&.email || true,
+      address_line: current_user&.address&.line || 'Address',
+      city: current_user&.address&.city || true,
+      state: current_user&.address&.state || true,
+      zip: current_user&.address&.zip || true,
+      subject: true,
+      body: true
+    }
   end
 end
