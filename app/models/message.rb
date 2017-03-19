1d6f424bf13e77dd120b599e5a94275cc63ed6fb
soccerkevin <soccerkevint11@gmail.com>
2017-03-18 23:01:11 -0700

    validate presence of message attributes

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,4 +1,5 @@
 class Message < ActiveRecord::Base
   include ActiveModel::Validations
   has_and_belongs_to_many :legislators
+  validates_presence_of :subject, :body, :name, :email, :address_line, :city, :state, :zip
 end
