9c53b6eccaa8b469af7e2d5c6c822f5cb51bf7f0
soccerkevin <soccerkevint11@gmail.com>
2017-03-23 15:06:48 -0700

    allow for 1 bio_id in email api

--- lib/vendor_api/congress_forms_api.rb
+++ lib/vendor_api/congress_forms_api.rb
@@ -9,12 +9,15 @@ module VendorAPI
     @@KEY_NAME = nil
 
     class << self
-      def get_form_elements(bio_ids)
+      def get_form(bio_ids)
         path = '/retrieve-form-elements'
-        # headers = { 'Content-Type': 'application/json' }
         headers = { 'Content-Type': 'application/x-www-form-urlencoded' }
-        body = { 'bio_ids' => bio_ids }
+        body = { 'bio_ids' => [bio_ids].flatten }
         response = post path, headers: headers, body: body
+      end
+
+      def get_form_topics(bio_ids)
+        response = get_form bio_ids
         topics = response.map{ |r| r.last['required_actions'].last['options_hash'] }
         array1 = topics.select{ |t| t.class == Hash }.map{ |h| h.keys }.flatten
         array2 = topics.select{ |t| t.class == Array }.flatten
@@ -23,13 +26,12 @@ module VendorAPI
         return []
       end
 
+      def fill_out_form()
+
+      end
+
       def get_elements_hash(bio_ids)
-        path = '/retrieve-form-elements'
-        # headers = { 'Content-Type': 'application/json' }
-        headers = { 'Content-Type': 'application/x-www-form-urlencoded' }
-        body = { 'bio_ids' => bio_ids }
-        response = post path, headers: headers, body: body
-        return response.map do |k,v|
+        get_form(bio_ids).map do |k,v|
           begin
             topics = v['required_actions'].select{|field| field['value'].downcase.include?('topic') }.first['options_hash']
             t = (topics.class == Hash ? topics.keys : topics).flatten
