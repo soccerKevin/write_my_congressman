a83ddd07f46ef225182655e525f860560408437e
soccerkevin <soccerkevint11@gmail.com>
2017-03-25 14:39:06 -0700

    load lib on load

--- config/application.rb
+++ config/application.rb
@@ -23,5 +23,7 @@ module Write
     # Do not swallow errors in after_commit/after_rollback callbacks.
     # config.autoload_paths += %W(#{config.root}/lib/vendor_api)
     config.active_record.raise_in_transactional_callbacks = true
+
+    config.autoload_paths << "#{Rails.root}/lib"
   end
 end
